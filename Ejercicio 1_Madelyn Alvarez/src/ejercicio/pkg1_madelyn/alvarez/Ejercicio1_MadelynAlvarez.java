9dcb8b1959e2cb86245afa450916e02fdb3512a8
madelyn-alvarez <37255171+madelyn-alvarez@users.noreply.github.com>
2018-03-11 21:01:19 -0600

    Add files via upload

---
+++ Ejercicio 1_Madelyn Alvarez/src/ejercicio/pkg1_madelyn/alvarez/Ejercicio1_MadelynAlvarez.java
@@ -0,0 +1,16 @@
+
+// Madelyn Scarleth Andino Alvarez  #2014-300-600-98
+// Crear el código que imprima la siguiente salida: "Hola, soy Arnol Gutiérrez" (Usar su
+//nombre)
+package ejercicio.pkg1_madelyn.alvarez;
+
+
+public class Ejercicio1_MadelynAlvarez {
+
+      public static void main(String[] args) {
+        System.out.println("Hola, Soy Madelyn Alvarez");
+          
+    }
+    
+}
+
