bd7086bc018cc89c9ff426bee44e956e95785909
madelyn-alvarez <37255171+madelyn-alvarez@users.noreply.github.com>
2018-03-11 21:02:54 -0600

    Add files via upload

---
+++ Ejercicio 4_Madelyn Alvarez/src/ejercicio/pkg4_madelyn/alvarez/Ejercicio4_MadelynAlvarez.java
@@ -0,0 +1,40 @@
+
+//Madelyn Scarleth Andino Alvarez #2014-300-600-98
+//
+// Crear un arrelo que guarde e imprima 10 nombres de tus compañeros de la clase
+
+package ejercicio.pkg4_madelyn.alvarez;
+
+public class Ejercicio4_MadelynAlvarez {
+    
+  public static void main(String arg[]){
+      
+      // Arreglo
+      String[] NombreCompañero = new String [10];
+      
+      NombreCompañero[0] = "Suyapa Maria Rivera Mejia";
+      NombreCompañero[1] = "Marilyn Yesenia Medina";
+      NombreCompañero[2] = "Selvin Mauricio Vivas Murillo";
+      NombreCompañero[3] = "Carlos Rene Andino Centeno";
+      NombreCompañero[4] = "Evelyn Claudina Diaz Perez";
+      NombreCompañero[5] = "Rolando Octavio Navarro Chirinos";
+      NombreCompañero[6] = "Wilmer Antonio Cerrato Sanchez";
+      NombreCompañero[7] = "Francisco Javier Morales Oseguera";
+      NombreCompañero[8] = "Ceily Estefania Araya Acevedo";
+      NombreCompañero[9] = "Grosvin Ivan Pineda Arias";
+      
+      //Imprimir en Pantalla Nombres de Compañeros de Clases
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[0]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[1]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[2]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[3]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[4]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[5]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[6]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[7]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[8]);
+      System.out.println("Nombre Compañero(a): " + NombreCompañero[9]);
+  }
+}
+    
+
