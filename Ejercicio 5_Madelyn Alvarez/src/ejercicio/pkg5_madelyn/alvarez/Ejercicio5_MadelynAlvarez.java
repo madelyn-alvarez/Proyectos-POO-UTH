993e7018d503a6f6f66e7a4a4a01c084e50ae3ee
Madelyn A. Alvarez <37255171+madelyn-alvarez@users.noreply.github.com>
2018-03-13 17:16:56 -0600

    Add files via upload

---
+++ Ejercicio 5_Madelyn Alvarez/src/ejercicio/pkg5_madelyn/alvarez/Ejercicio5_MadelynAlvarez.java
@@ -0,0 +1,55 @@
+
+// Madelyn Scarleth Andino Alvarez #2014-300-600-98
+// rear un arreglo multidimensional que guarde más datos personales tus compañeros de
+//clase (nombre, apellido, carrera, lugarTrabajo), tomando como referencia de la
+//información colocada en el foro Conociendonos.
+package ejercicio.pkg5_madelyn.alvarez;
+
+public class Ejercicio5_MadelynAlvarez {
+
+     public static void main(String[] args) {
+         
+       
+         
+          String[] nombre = new String [2];
+         
+      nombre[0] = "Daniel Medina";
+      nombre[1] = "Monica Jiz";
+      
+         String[] carrera = new String [2];
+      carrera[0] = "Computacion";
+      carrera[1] = "Electronica";
+     
+      
+      
+         String[] trabajo = new String [2];
+    
+      trabajo[0] = "TEST";
+      trabajo[1] = "IMSA";
+      
+      
+         
+      
+         for(int a=1; a<2; a++){
+              System.out.println("Nombre:" +  nombre[0]);
+              System.out.println("Carrera:" + carrera[0]);
+              System.out.println("Labora:" + trabajo[0]);
+              System.out.println("------------------");
+                                }
+         for(int b=1; b<2; b++){
+              System.out.println("Nombre:" +  nombre[1]);
+              System.out.println("Carrera:" + carrera[1]);
+              System.out.println("Labora:" + trabajo[1]);
+              System.out.println("------------------");
+                               }
+              
+              
+          
+        }
+}
+
+        
+
+
+
+     
