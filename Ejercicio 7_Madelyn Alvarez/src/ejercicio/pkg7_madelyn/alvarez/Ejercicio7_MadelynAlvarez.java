25c53b1a8689f51376095800ad453270def2c57f
Madelyn A. Alvarez <37255171+madelyn-alvarez@users.noreply.github.com>
2018-03-13 17:18:06 -0600

    Add files via upload

---
+++ Ejercicio 7_Madelyn Alvarez/src/ejercicio/pkg7_madelyn/alvarez/Ejercicio7_MadelynAlvarez.java
@@ -0,0 +1,17 @@
+
+//Madelyn Scarleth Andino Alvarez # 2014-300-600-98
+//Escribe un programa que imprima mediante un ciclo los números pares del 2 al 100
+
+package ejercicio.pkg7_madelyn.alvarez;
+
+public class Ejercicio7_MadelynAlvarez {
+
+    public static void main(String[] args) {
+        
+        for (int i=2;  i<=100;  i+=2){
+          System.out.println(i);
+        }
+        
+    }
+    
+}
